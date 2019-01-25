e37959dc43393cf3624ac28bf01088533c42b3e6
Fourzifer <30759328+Fourzifer@users.noreply.github.com>
2019-01-26 00:44:50 +0100

    HP bar

---
+++ Dragongameggj2019/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    Image healthBar;
+    float maxHealth = 160f;
+    public static float health;
+
+    void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+}
