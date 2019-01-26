254fc3aec8793defee97264961f19d7242269d37
Fourzifer <30759328+Fourzifer@users.noreply.github.com>
2019-01-26 13:06:51 +0100

    Back again

--- Dragongameggj2019/Assets/Scripts/Move_and_attack.cs
+++ Dragongameggj2019/Assets/Scripts/Move_and_attack.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class Move_and_attack : MonoBehaviour
 {
-    gggameManager GameManager;
     float dirX, dirY;
     public float moveSpeed = 5f;
-    public Vector3 moveVector;
     Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
-        GameManager = gameController.GetComponent<gggameManager>();
+
     }
 
     void Update()
     {
-        transform.Translate(GameManager.moveVector * GameManager.moveSpeed * Time.deltaTime);
+       
         dirX = Input.GetAxis("Horizontal");
         dirY = Input.GetAxis("Vertical");
     }
