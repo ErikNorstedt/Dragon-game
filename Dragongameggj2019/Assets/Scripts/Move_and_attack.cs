df7e612c7d8614c3a87efd24b22a07dbf3efc7e7
Fourzifer <30759328+Fourzifer@users.noreply.github.com>
2019-01-26 13:43:42 +0100

    Moving script con

--- Dragongameggj2019/Assets/Scripts/Move_and_attack.cs
+++ Dragongameggj2019/Assets/Scripts/Move_and_attack.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class Move_and_attack : MonoBehaviour
 {
-    float dirX, dirY;
-    public float moveSpeed = 5f;
+    public GameObject blood;
+
+    public float moveSpeed = 3f;
     Rigidbody2D rb;
+    Vector3 localScale;
+    bool movingRight = true;
+    Transform leftWayPoint, rightWayPoint;
 
     void Start()
     {
+        localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
-
+        leftWayPoint = GameObject.Find("LeftWayPoint").GetComponent<Transform>();
+        rightWayPoint = GameObject.Find("RightWayPoint").GetComponent<Transform>();
     }
 
     void Update()
     {
-       
-        dirX = Input.GetAxis("Horizontal");
-        dirY = Input.GetAxis("Vertical");
+        if (transform.position.x > rightWayPoint.position.x)
+            movingRight = false;
+        if (transform.position.x < leftWayPoint.position.x)
+            movingRight = true;
+
+        if (movingRight)
+            moveRight();
+        else
+            moveLeft();
     }
 
-    void FixedUpdate()
+    void moveRight()
     {
-        rb.velocity = new Vector2(dirX * moveSpeed, dirY * moveSpeed);
+        movingRight = false;
+        localScale.x = 1;
+        transform.localScale = localScale;
+        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
+    }
+    void moveLeft()
+    {
+        movingRight = false;
+        localScale.x = -1;
+        transform.localScale = localScale;
+        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
     }
 }
